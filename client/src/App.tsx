61106606cb83a01faefc47d9e7963284215e3b82
Tiong Shan Kai <tiongshankai97@gmail.com>
2024-01-26 21:20:07 +0800

    feat: inital client page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+import { DataTable } from "./components/DataTable";
+import { PaginationBar } from "./components/PaginationBar";
+import { QueryForm } from "./components/QueryForm";
+
 function App() {
+  
   return (
-    <>
-      <div className="text-5xl">Hello world</div>
-    </>
+    <div className="flex flex-col gap-2 justify-center items-center p-10">
+      <h1 className="text-4xl text-center font-medium">
+        Uniswap V3 ETH/USDC Transaction Fee Tracker
+      </h1>
+      <QueryForm />
+      <DataTable />
+      <PaginationBar />
+    </div>
   );
 }
 
